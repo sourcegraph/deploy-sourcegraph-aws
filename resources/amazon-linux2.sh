fa9dd6d8f815e19be049acd15724305b0e54af73
BolajiOlajide <cooproton@gmail.com>
2024-01-25 13:59:00 -0800

    release: sourcegraph@5.2.7

--- resources/amazon-linux2.sh
+++ resources/amazon-linux2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=5.2.6
+export SOURCEGRAPH_VERSION=5.2.7
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
 export PATH=$PATH:/usr/local/bin
